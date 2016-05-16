4220ef6d7b02d8b454eb322d0df970fd07337386
zafezo <nazarslbdn@gmail.com>
2016-05-16 20:30:51 +0200

    SYNC Update list of message, Image handler

--- src/modules/app/chat_room/chatRoomController.js
+++ src/modules/app/chat_room/chatRoomController.js
@@ -1,14 +1,19 @@
 /**
  * Created by swen on 5/16/16.
  */
-module.exports = function ($scope,messageService,sessionService) {
+module.exports = function ($scope,messageService,sessionService,$interval) {
     $scope.messages = messageService.getAll();
+    $interval(function () {
+        $scope.messages = messageService.getAll()
+    },3000);
     $scope.send = function () {
         var message ={
             user:{
                 name:sessionService.getUser().name
             },
-            message: $scope.data.message
+            message: $scope.data.message,
+            date: new Date(),
+            image:"assets/images/cat1.png"
         };
         console.log(sessionService.getUser().name);
         messageService.send(message);
